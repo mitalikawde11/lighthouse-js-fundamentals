768cf02de41362869c7e9c99baa1a6028a19934b
Mitali <mitali.kawde11@gmail.com>
2023-02-03 08:14:18 -0800

    check the age for deciding school

--- conditionals.js
+++ conditionals.js
@@ -2,7 +2,8 @@ const temperature = -41;
 const isCitizen = true;
 const age = 30;
 const raining = false;
-/*
+
+
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
@@ -11,7 +12,7 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 console.log("Now you're ready to go outside!");
-*/
+/*
 //if statement using logical operators
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
@@ -23,4 +24,16 @@ if (temperature < -40 || temperature > 40) {
 
 if(!raining){
   console.log("Leave your umbrella at home!");
-}
+} 
+*/
+// Checking the age to help learners for deciding which school they attend.
+if (age >=1 && age <=12) {
+  console.log("I am " + age + ". Which school should I go to?");
+  console.log("Elementary School");
+} else if (age >= 13 && age <= 18) {
+  console.log("I am " + age + ". Which school should I go to?");
+  console.log("Secondary School");
+} else {
+  console.log("I am " + age + ". Which school should I go to?");
+  console.log("Lighthouse Labs");
+} 
