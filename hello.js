3224b9ff26054c318434411a63a5255dcb9a0aef
Mitali <mitali.kawde11@gmail.com>
2023-02-04 14:51:32 -0800

    difference between return & console.log

--- hello.js
+++ hello.js
@@ -2,6 +2,8 @@ const sayHello = function (name){
   console.log("Hello, " + name);
 };
 sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
-
+const returnSayHello = function (name){
+  return "Hello " + name;
+};
+const greeting = returnSayHello("John");
+console.log(greeting);
